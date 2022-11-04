bb108befb4df567467fe6f748c40f85e50d0baac
VTHARUN01 <v.tharun2589@gmail.com>
2022-11-04 19:33:50 +0530

    cookie commit

--- types/return.ts
+++ types/return.ts
@@ -2,14 +2,10 @@ import { Field, ObjectType } from "type-graphql";
 
 @ObjectType()
 class LoginResponse {
+  @Field(() => String)
+  token: string | undefined;
   @Field(() => Boolean)
   success: boolean | undefined;
-
-  @Field(() => String)
-  username: string | null | undefined;
-
-  @Field(() => String)
-  tags: string | null | undefined;
 }
 
 export default LoginResponse;
