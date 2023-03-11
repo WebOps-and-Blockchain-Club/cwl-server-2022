85a47012e791f3f820f18546f4fc4f187eb93ebc
VTHARUN01 <v.tharun2589@gmail.com>
2023-03-11 23:23:45 +0530

    backup file

--- entities/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const issue_1 = __importDefault(require("./issue"));
-const volunteer_1 = __importDefault(require("./volunteer"));
-const waterData_1 = __importDefault(require("./waterData"));
-exports.default = [issue_1.default, volunteer_1.default, waterData_1.default];
--- resolver/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const volunteer_1 = require("./volunteer");
-const waterData_1 = require("./waterData");
-const issue_1 = require("./issue");
-exports.default = [volunteer_1.VolunteerResolver, waterData_1.WaterDataResolver, issue_1.IssueResolver];
